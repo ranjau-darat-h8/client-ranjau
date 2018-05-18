3fd3984a164bf81c0c5d459e74fbd264dba8c750
marcosumali <marco.sumali90@gmail.com>
2018-05-18 11:09:53 +0700

    add room features

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
+      <router-link to="/lobby">Lobby</router-link> |
       <router-link to="/room">Room</router-link>
     </div>
     <router-view/>
--- src/store.js
+++ src/store.js
@@ -3,24 +3,30 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import VueFire from 'vuefire'
 import firebase from 'firebase'
+import router from './router'
 
 Vue.use(VueFire)
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-    roomsList: []
+    roomsList: [],
+    room: ''
   },
   mutations: {
     getRooms (state, payload) {
       state.roomsList = payload
+    },
+    getRoom (state, payload) {
+      state.room = payload
+      console.log('after', state.room)
     }
   },
   actions: {
     getRooms ({commit}) {
       firebase.database().ref('Rooms').on('value', function (snapshot) {
-        console.log(snapshot.val())
         let roomsList = snapshot.val()
+        console.log('all rooms', roomsList)
         commit('getRooms', roomsList)
       })
     },
@@ -28,7 +34,7 @@ export default new Vuex.Store({
       // console.log('masuk actions')
       // let newPostKey = firebase.database().ref('Rooms').push().key
       let username = localStorage.getItem('username')
-      firebase.database().ref('Rooms').push({
+      let room = firebase.database().ref('Rooms').push({
         Board: {
           Board0: {
             show: 'blank',
@@ -74,7 +80,18 @@ export default new Vuex.Store({
           turn: true
         }
       })
+      // console.log('ini room', room.key)
+      let token = room.key
+      localStorage.setItem('token', token)
+      setTimeout(function () { router.push('room') }, 3000)
+    },
+    getRoom ({commit}) {
+      let token = localStorage.getItem('token')
+      firebase.database().ref('Rooms/' + token).on('value', function (snapshot) {
+        let room = snapshot.val()
+        console.log(room)
+        commit('getRoom', room)
+      })
     }
-
   }
 })

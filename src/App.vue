52c43b3be255c247cbd2db138c0830798b264faf
AdhityaRdt <rahmanadhitya2201@gmail.com>
2018-05-18 15:54:51 +0700

    finalize

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
+      <!-- <router-link to="/about">About</router-link> | -->
       <router-link to="/lobby">Lobby</router-link> |
       <!-- <router-link to="/room">Room</router-link> -->
       <router-link to="/game">Game</router-link>
